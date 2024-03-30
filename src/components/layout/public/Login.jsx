8114de39e6067c22170c7a5b3ac85874b376576b
bobkings <robertoc-95@hotmail.com>
2024-03-30 10:51:48 -0600

    se guardan ultimos cambios

--- src/components/layout/public/Login.jsx
+++ src/components/layout/public/Login.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faKey, faUser} from "@fortawesome/free-solid-svg-icons"
+import { useForm } from '../../../hooks/useForm'
+import { Global } from '../../../helpers/Global';
+import useAuth from '../../../hooks/useAuth';
+
 
 export const Login = () => {
+
+  const { form, changed } = useForm({});
+  const [saved, setSaved] = useState("not_sent");
+
+  const {setAuth} = useAuth();
   
+  const loginUser = async (e) => {
+    e.preventDefault();
+
+    //datos del formulario
+    let userToLogin = form;
+
+    //peticion al backend
+    const request = await fetch(Global.url+"user/login",{
+      method: "POST",
+      body: JSON.stringify(userToLogin),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    const data = await request.json();
+
+    //persistir los datos en el navegador
+    if(data.ok){
+      //persistir los datos en el navegador
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user", JSON.stringify(data.user));
+
+      setSaved("logged");
+      
+      //set datos de usuario en el auth
+      setAuth(data.user);
+
+      //redireccion 
+      setTimeout(()=>{
+        window.location.reload();
+      }, 1000)
+
+    }else{
+      setSaved("error")
+    }
+  }
 
   return (
 <div className="container-login">
@@ -16,16 +62,16 @@ export const Login = () => {
           </div>
         </div>
         <div className="card-body">
-          <form>
+          <form onSubmit={loginUser}>
             
             <div className="input-group input-group-prepend form-group mb-4">
               <span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
-              <input type="text" className="form-control" placeholder="usuario" />
+              <input type="text" className="form-control" placeholder="usuario" name="user" onChange={changed} />
             </div>
             
             <div className="input-group input-group-prepend form-group mb-4">
               <span className="input-group-text"><FontAwesomeIcon icon={faKey} /></span>              
-              <input type="password" className="form-control" placeholder="contraseña" />
+              <input type="password" className="form-control" placeholder="contraseña" name="password" onChange={changed} />
             </div>
             
             <div className="form-group mb-4 d-flex justify-content-center">
---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,60 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { Global } from '../helpers/Global';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    authUser();
+  }, [])
+
+  const authUser = async () => {
+    //sacar datos del usuario identificado del localstorage
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+
+    //comprobar si tengo token y el user
+    if (!token || !user) {
+      setLoading(false);
+      return false;
+    }
+
+    //transformar datos a objeto javascript
+    const userObj = JSON.parse(user);
+    const userId = userObj.id;
+
+    //peticion ajax al backend que compruebe el token
+
+    //devolver datos del usuario
+    const request = await fetch(Global.url + "user/list-one/" + userId, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token
+      }
+    })
+
+    const data = await request.json();
+
+    //setear el estado de auth
+    setAuth(data.user);
+    setLoading(false);
+  }
+
+  return (<AuthContext.Provider
+    value={
+      {
+        auth,
+        setAuth,
+        loading
+      }
+    }
+  >
+    {children}
+  </AuthContext.Provider>)
+}
+
+export default AuthContext;
